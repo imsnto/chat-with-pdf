d8346e2bad19d7caa811fa498df9ead4eaccd4e9
Solaiman Hossain <slmnsnto@gmail.com>
2025-04-24 10:10:36 +0600

    feat: add faiss vector database

--- src/vectordb.py
+++ src/vectordb.py
@@ -1,6 +1,10 @@
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 import chromadb
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
 
 def to_document(chunk):
     return Document(page_content=chunk)
@@ -10,18 +14,27 @@ def get_vector_store(chunks, embeddings):
     # Convert chunks to documents first
     documents = [to_document(chunk) for chunk in chunks]
 
-    # Configure persistent Chroma with a valid persist_directory
-    persist_directory = "./chroma_db"
-    client_settings = chromadb.config.Settings(
-        persist_directory=persist_directory,
-        is_persistent=True
-    )
 
-    # Initialize Chroma vector store
-    vector_store = Chroma(
+    # # Configure persistent Chroma with a valid persist_directory
+    # persist_directory = "./chroma_db"
+    # client_settings = chromadb.config.Settings(
+    #     #persist_directory=persist_directory,
+    #     is_persistent=False
+    # )
+    #
+    # # Initialize Chroma vector store
+    # vector_store = Chroma(
+    #     embedding_function=embeddings,
+    #     client_settings=client_settings,
+    #     collection_name="my_collection"
+    # )
+
+    index = faiss.IndexFlatL2(len(embeddings.embed_query("hello world")))
+    vector_store = FAISS(
         embedding_function=embeddings,
-        client_settings=client_settings,
-        collection_name="my_collection"
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={},
     )
 
     # Generate IDs as strings from 0 to len(documents)-1
